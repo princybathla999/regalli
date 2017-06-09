1046e6dda9623069c8652c8ed6c52b32e2a92073
Alpha Blog <princybathla999@yahoo.com>
2017-06-09 18:41:05 -0400

    Readme and changing the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :games
   devise_for :users
-  root to: "home#index"
-  get '/history', to: 'home#history'
-  get '/log',     to: 'home#log'
+  root to: 'games#rank'
   get '/user',     to: 'home#user'
   get 'rank',     to: 'games#rank'
 
